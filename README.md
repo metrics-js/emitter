671a90b0a15d5760aac72dcb2ab14c16b8b1adb4
Trygve Lie <post@trygve-lie.com>
2018-12-27 00:33:10 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# emitter
+# process
+
+Module for collecting different process and system metrics and providing them as a metric stream
---
+++ test/emitter.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const tap = require('tap');
+const stream = require('readable-stream');
+const Emitter = require('../');
+
+const destObjectStream = (done) => {
+    const arr = [];
+
+    const dStream = new stream.Writable({
+        objectMode: true,
+        write(chunk, encoding, callback) {
+            arr.push(chunk);
+            callback();
+        },
+    });
+
+    dStream.on('finish', () => {
+        done(arr);
+    });
+
+    return dStream;
+};
+
+/**
+ * Constructor
+ */
+
+tap.test('Constructor() - object type - should be MetricsEmitter', (t) => {
+    const emitter = new Emitter();
+    t.equal(Object.prototype.toString.call(emitter), '[object MetricsEmitter]');
+    t.end();
+});
