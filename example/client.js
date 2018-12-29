1126f76baf028cc30b8896bd34d180dd0b1427cb
Trygve Lie <post@trygve-lie.com>
2018-12-29 11:01:08 +0100

    Updated example to use @metrics/client

--- example/client.js
+++ example/client.js
@@ -1,27 +1,20 @@
 'use strict';
 
-const stream = require('readable-stream');
+const Client = require('@metrics/client');
 const Emitter = require('../');
 
-const src = new stream.Readable({
-    objectMode: true,
-    read() {
-        // do nothing here
-    },
-});
-
 const emitter = new Emitter('udp');
-src.pipe(emitter);
+const client = new Client();
+
+client.pipe(emitter);
 
 let counter = 0;
 setInterval(() => {
-    const msg = {
+    client.metric({
         name: 'foo',
         description: 'bar',
         value: counter,
-        timestamp: Date.now() / 1000,
-    };
-    src.push(msg);
+    });
 
     counter += 1;
 }, 1000);
